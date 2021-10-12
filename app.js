f20ad9bc1a3afbda42bf32ca3a8b944777083151
vijay <vijaysm97@gamil.com>
2021-10-12 08:33:28 +0530

    hosted in firebase

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use('/api', userRoutes.routes)
 // var admin = require('firebase-admin');
 // var serviceAccount = require("./key.json");

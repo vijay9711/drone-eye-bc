79f2d0ed17169a1382c4efc4e3e1711e5464558a
vijay <vijaysm97@gamil.com>
2021-10-12 08:36:59 +0530

    hosted in firebase

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const bodyParser = require('body-parser');
 const config = require('./config');
 const userRoutes = require('./routes/usersRoute');
 const app = express();
-
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+  
+app.use(cors(corsOpts));
 app.use(express.json());
-app.use(cors());
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 app.use('/api', userRoutes.routes)
 // var admin = require('firebase-admin');
 // var serviceAccount = require("./key.json");
